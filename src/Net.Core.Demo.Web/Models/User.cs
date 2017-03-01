a34a44bfbff86b234ceebb64a43b3b4d47eebbe8
wrb94 <209150@student.pwr.edu.pl>
2017-03-01 19:46:44 +0100

    Added swagger

--- src/Net.Core.Demo.Web/Models/User.cs
+++ src/Net.Core.Demo.Web/Models/User.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Net.Core.Demo.Web.Models
+﻿namespace Net.Core.Demo.Web.Models
 {
     public class User
     {
